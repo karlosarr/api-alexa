68eb3503c490d40a11089494e52933e85fd409b3
Carlos <karlosarr@gmail.com>
2020-05-26 00:01:51 -0500

    changed port

--- skillBuilder.js
+++ skillBuilder.js
@@ -29,8 +29,7 @@ const HelloWorldIntentHandler = {
       Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
   },
   handle(handlerInput) {
-    //const speakOutput = handlerInput.t('HELLO_MSG');
-    const speakOutput = "¡Hola!"
+    const speakOutput = handlerInput.t('HELLO_MSG');
 
     return handlerInput.responseBuilder
       .speak(speakOutput)
