443ac36d60893afc8ad70e3e37d5698438ce2c12
Carlos <karlosarr@gmail.com>
2020-05-25 23:11:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {
+  ExpressAdapter
+} = require('ask-sdk-express-adapter');
+
+const app = express();
+const skillBuilder = require('./skillBuilder.js').skillBuilder;
+const skill = skillBuilder.create();
+const adapter = new ExpressAdapter(skill, false, false);
+console.log('indes.js');
+app.post('/', adapter.getRequestHandlers());
+app.listen(3000);
+
