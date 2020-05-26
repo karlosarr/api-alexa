67282f36a2d73fdfc658982191b0a9f59b84dff1
Carlos <karlosarr@gmail.com>
2020-05-25 23:48:46 -0500

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const skillBuilder = require('./skillBuilder.js').skillBuilder;
 const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, false, false);
-console.log('indes.js');
+console.log('index.js');
 app.post('/', adapter.getRequestHandlers());
 app.listen(3000);
 
